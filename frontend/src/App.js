97cb0ff185b94fa13ec9c5999d237f5a6f4a0622
Mahdi Rezai <mahdirezai@Macbook-m.local>
2021-04-03 12:37:43 +0430

    React Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return <>
+    <Container>
+      <h1>
+        Hello Word!
+      </h1>  
+    </Container>
+  </>;
+}
+
+export default App;
