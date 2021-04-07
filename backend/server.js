487358cc7de1c5690f35f0b8844ab0d7654aaa48
Mahdi Rezai <mahdirezai@Macbook-m.local>
2021-04-07 15:03:42 +0430

    implementing User Authentication in backend

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
 
-// import products from './data/products.js';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -13,21 +13,14 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.json('API is running...');
 });
 
-// app.get('/api/products', (req, res) => {
-//     res.json(products);
-// });
-
-// app.get('/api/products/:id', (req, res) => {
-//     const product = products.find(p => p._id === req.params.id);
-
-//     res.json(product);
-// });
-
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
